c3b236cb61f4eb87997530d5be87a536a5a0a289
Wiktor Szlegier <22wiktors@gmail.com>
2020-04-05 18:23:11 +0200

    Updates

---
+++ precache-manifest.7198d3fe8d322b78719a1f856dd1a9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8134abfab7a5d5c89ddf24b7825a1465",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "dc3cce689ec89a041ffd",
+    "url": "/NewsApp/static/js/2.eb8b241e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewsApp/static/js/2.eb8b241e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab93b0dfd7672a22238",
+    "url": "/NewsApp/static/js/main.99c2b889.chunk.js"
+  },
+  {
+    "revision": "813b79d771540678f0c8",
+    "url": "/NewsApp/static/js/runtime-main.12fd14a1.js"
+  },
+  {
+    "revision": "fca516ae9a017df87e9c6593118d3069",
+    "url": "/NewsApp/static/media/news.fca516ae.svg"
+  }
+]);
